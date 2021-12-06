5421faf055707542fb49cfb01a3e1fbc5ce22e82
CJYLZS <691086891@qq.com>
2021-12-06 19:09:49 +0800

    complete hw3

--- chapter31/mutex-nostarve.c
+++ chapter31/mutex-nostarve.c
@@ -12,46 +12,61 @@
 //
 
 typedef struct __ns_mutex_t {
-    sem_t mutex;
-    sem_t barrier;
+    int turn;
+    int count;
     sem_t lock;
-    int num_threads;
+    sem_t turnlock;
 } ns_mutex_t;
 
 ns_mutex_t m;
 
-void ns_mutex_init(ns_mutex_t *m, int num_threads) {
-    sem_init(&m->barrier, 0, -num_threads + 1);
-    sem_init(&m->mutex, 0, 1);//互斥锁
+void ns_mutex_init(ns_mutex_t *m) {
+    sem_init(&m->lock, 0, 1);
+    sem_init(&m->turnlock, 0, 1);
+    m->turn = 1;
+    m->count = 0;
 }
 
 void ns_mutex_acquire(ns_mutex_t *m) {
-    sem_wait(&m->lock);
+    sem_wait(&m->turnlock);
+    int turn = ++m->count;
+    while (turn != m->turn) {
+        sem_post(&m->turnlock);
+        sem_wait(&m->lock);
+        sem_wait(&m->turnlock);
+    }
+    sem_post(&m->turnlock);
 }
 
 void ns_mutex_release(ns_mutex_t *m) {
+    sem_wait(&m->turnlock);
+    m->turn++;
     sem_post(&m->lock);
-
-    sem_post(&m->barrier);
-    sem_wait(&m->barrier);
-
-    sem_wait(&m->mutex);
-    for (int i = 0; i < m->num_threads; i++) {
-        sem_post(&m->barrier);
-        //确认所有线程都执行过临界区后再继续执行
-    }
-    sem_post(&m->mutex);
+    sem_post(&m->turnlock);
 }
 
 
 void *worker(void *arg) {
+    printf("Started: %lu\n", pthread_self());
+    ns_mutex_acquire(&m);
+    printf("Got lock: %lu\n", pthread_self());
+    ns_mutex_release(&m);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     printf("parent: begin\n");
+    ns_mutex_init(&m);
+    pthread_t threads[10];
+    for(int i = 0; i < 10; i++) {
+        Pthread_create(&threads[i], NULL, worker, NULL);
+    }
+    for(int i = 0; i < 10; i++) {
+        Pthread_join(threads[i], NULL);
+    }
     printf("parent: end\n");
     return 0;
 }
+
 
 
